4df10b8897a72822fb7211b9a79c228e16321cae
For-Fox <design.oksana@gmail.com>
2024-09-26 16:17:25 +0200

    Add files via upload

---
+++ hw02/Test.java
@@ -0,0 +1,22 @@
+package homework.hw02;
+
+public class Test {
+        public static void main(String[] args) {
+            char oxchar = 'G';
+            int oxint = 89;
+            byte oxbyte = 4;
+            short oxshort = 56;
+            float oxfloat = 4.7333436F;
+            double oxdouble = 4.355453532;
+            long oxlong = 12121;
+
+            System.out.println("Character: " + oxchar);
+            System.out.println("Integer: " + oxint);
+            System.out.println("Byte: " + oxbyte);
+            System.out.println("Short: " + oxshort);
+            System.out.println("Float: " + oxfloat);
+            System.out.println("Double: " + oxdouble);
+            System.out.println("Long: " + oxlong);
+
+        }
+    }
---
+++ hw02/Test2.java
@@ -0,0 +1,10 @@
+package homework.hw02;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 5;
+        System.out.println("Число 345 -> " + num1 + "," + num2 + "," +num3);
+    }
+}
