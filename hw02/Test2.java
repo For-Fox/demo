32feccb3caded5eb929ebebd8bc1b73c1622e1d3
For-Fox <design.oksana@gmail.com>
2024-09-28 13:10:39 +0200

    Update Test2.java

--- hw02/Test2.java
+++ hw02/Test2.java
@@ -2,9 +2,11 @@ package homework.hw02;
 
 public class Test2 {
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 4;
-        int num3 = 5;
+        int num = 345;
+
+        int num1 = num / 100;
+        int num2 = num / 10 % 10;
+        int num3 = num % 10;
         System.out.println("Число 345 -> " + num1 + "," + num2 + "," +num3);
     }
 }
