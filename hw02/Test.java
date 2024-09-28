2893175f00699fbab765c45b64a8849c6c2cc7e0
For-Fox <design.oksana@gmail.com>
2024-09-28 13:11:02 +0200

    Update Test.java

--- hw02/Test.java
+++ hw02/Test.java
@@ -2,21 +2,31 @@ package homework.hw02;
 
 public class Test {
         public static void main(String[] args) {
-            char oxchar = 'G';
-            int oxint = 89;
-            byte oxbyte = 4;
-            short oxshort = 56;
-            float oxfloat = 4.7333436F;
-            double oxdouble = 4.355453532;
-            long oxlong = 12121;
+            char oxChar = 'G';
+            int oxInt = 89;
+            byte oxByte = 4;
+            short oxShort = 56;
+            float oxFloat = 4.7333436F;
+            double oxDouble = 4.355453532;
+            long oxLong = 12121;
+            boolean oxBool = true;
+            String oxString = "Я учла комментарии";
+            Integer oxInteger = Integer.valueOf(oxInt);
+            Double oxDoubleOb = Double.valueOf(oxDouble);
+            Character oxCharOb = Character.valueOf(oxChar);
 
-            System.out.println("Character: " + oxchar);
-            System.out.println("Integer: " + oxint);
-            System.out.println("Byte: " + oxbyte);
-            System.out.println("Short: " + oxshort);
-            System.out.println("Float: " + oxfloat);
-            System.out.println("Double: " + oxdouble);
-            System.out.println("Long: " + oxlong);
+            System.out.println("Character: " + oxChar);
+            System.out.println("Integer: " + oxInt);
+            System.out.println("Byte: " + oxByte);
+            System.out.println("Short: " + oxShort);
+            System.out.println("Float: " + oxFloat);
+            System.out.println("Double: " + oxDouble);
+            System.out.println("Long: " + oxLong);
+            System.out.println("Boolean: " + oxBool);
+            System.out.println("String: " + oxString);
+            System.out.println("Integer: " + oxInteger);
+            System.out.println("Double: " + oxDoubleOb);
+            System.out.println("Character: " + oxCharOb);
 
         }
     }
