c4eabd7d34bfd35658fce43a6717c0fe962ba1db
For-Fox <design.oksana@gmail.com>
2024-11-12 09:31:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+//let a = 5, b = 10;
+//console.log(a + b);
+
+//let a = 20,b = 7;
+//console.log(a - b);
+
+//let a = 8, b = 3;
+//console.log(a * b);
+
+//let a = 15, b = 3;
+//console.log(a / b);
+
+//let a = 17, b = 5;
+//console.log(a % b);
+
+//let x = 17;
+//console.log(++x);
+//console.log(x += 1);
+
+//let y = 15;
+//console.log(--y);
+//console.log(y -= 1);
+
+//console.log(2 ** 4);
+
+//let z = 10;
+//console.log(z += 5);
+
+//let a = 6;
+//console.log(a *= 3);
+
+/*console.log(5 > 3); //true
+console.log(7 <= 7); //true
+console.log(10 == '10'); //true
+console.log(10 === '10'); //false
+console.log(0 === false); //false
+console.log('apple' > 'banana'); //false
+console.log(null == undefined); //true
+console.log(NaN == NaN); //false
+console.log(5 != '5'); //false
+console.log('5' !== 5); //true
+
+console.log(String(10)); //10
+console.log('5' * 2); // 10
+console.log(Number('20')); //20
+console.log(Number(true)); //1
+console.log(5 + '5'); // 55
+console.log(Number(undefined)); //NaN
+console.log(null + 5); //5
+console.log(parseInt('123abc')); //123
+console.log(Boolean(0)); //false
+console.log(String(false)); //false
+
+alert('Привет, мир!');
+
+let name = prompt('Введите ваше имя:');
+
+let isConfirmed = confirm('Вы уверены?');
+
+let num1 = Number(prompt('Введите первое число:'));
+let num2 = Number(prompt('Введите второе число:'));
+alert(`Сумма: ${num1 + num2}`);
+
+let x = 17;
+alert(x);
+
+let input = prompt('Введите что-то:');
+if (input === null) {
+    alert('Пользователь нажал Отмена');
+} else {
+    alert(`Вы ввели: ${input}`);
+}
+
+alert('Lorem ipsum dolor sit, amet consectetur adipisicing elit. \n Exercitationem cum placeat ad voluptates illum tenetur, rem veritatis corporis id non sunt ipsa harum deserunt veniam expedita quaerat iure nulla. Tenetur?\n
+   Voluptatum repudiandae earum eligendi consectetur quae nisi nulla nihil repellendus excepturi minus. Et voluptatibus omnis autem, pariatur quia quam similique, maiores distinctio officiis eveniet molestiae quisquam explicabo eius nobis dolor!\n
+   Vel eos, eveniet obcaecati asperiores ab veniam, cumque ipsa amet quasi, atque dolorum eaque facilis molestiae nostrum quam hic praesentium ? Vel non tenetur est hic praesentium quis, odit perspiciatis molestias.');
+
+let number = Number(prompt('Введите число:'));
+alert(`Квадрат числа: ${number ** 2}`);
+
+let result = confirm('Продолжить?');
+console.log(result);
+
+let age = prompt('Введите ваш возраст:', '5');*/
